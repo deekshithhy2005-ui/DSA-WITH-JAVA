a99b6505583b799dc004575a148676526f282153
Deekshith H Y <deekshithhy2005@gmail.com>
2025-09-11 00:27:48 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,13 @@
+public class Unary {
+        public static void main(String[] args) {
+            int a = 10;
+           // int b = ++a;
+           //int b = a++;
+           //int b = --a;
+           int b = a--;
+            System.out.println(a);
+            System.out.println(b);
+        }
+        }
+    
+
