89235d9c26eac7e41b69ffe560e89496f4455dfb
Deekshith H Y <deekshithhy2005@gmail.com>
2025-09-12 00:31:21 +0530

    Delete Logical.java

--- Logical.java
+++
@@ -1,8 +0,0 @@
-public class Logical {
-   
-        public static void main(String[] args) {
-           // System.out.println((5<2) && (6<3));//using and operator
-          // System.out.println((5>7) || (6>3));//using or operator
-          System.out.println(!(5>2));//uisng not operator
-                }
-            }
