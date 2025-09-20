d207e2dcbd055b84a46081c99c72fbcf1de16b47
Deekshith H Y <deekshithhy2005@gmail.com>
2025-09-20 19:49:40 +0530

    Add files via upload

---
+++ Functiones/Examplecallbyvalue.java
@@ -0,0 +1,12 @@
+public class Examplecallbyvalue {
+    public static void changeA(int a){
+           a=10;
+    }
+    public static void main(String[] args) {
+        int a=5;
+        changeA(a);
+        System.out.println(a);
+    }
+
+    }
+
